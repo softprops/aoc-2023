0a0b8dce6e34da875ca692a1c7d7f87168f899a8
softprops <d.tangren@gmail.com>
2023-12-06 01:18:45 -0500

    day 6 part 2

--- src/day06.zig
+++ src/day06.zig
@@ -54,6 +54,60 @@ fn part1(input: []const u8) !usize {
     return product;
 }
 
+fn part2(input: []const u8) !usize {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+    var lines = std.mem.split(u8, input, "\n");
+    var product: usize = 1;
+    var times = blk: {
+        const time_line = lines.next().?;
+        var parts = std.mem.split(u8, time_line, ":");
+        _ = parts.next().?;
+
+        var split = std.mem.split(u8, parts.next().?, " ");
+        var result = std.ArrayList(u8).init(allocator);
+        while (split.next()) |num| {
+            if (!std.mem.eql(u8, num, "")) {
+                try result.appendSlice(num);
+            }
+        }
+        break :blk result;
+    };
+    defer times.deinit();
+    var distances = blk: {
+        const distance_line = lines.next().?;
+        var parts = std.mem.split(u8, distance_line, ":");
+        _ = parts.next().?;
+        var split = std.mem.split(u8, parts.next().?, " ");
+        var result = std.ArrayList(u8).init(allocator);
+        while (split.next()) |num| {
+            if (!std.mem.eql(u8, num, "")) {
+                try result.appendSlice(num);
+            }
+        }
+        break :blk result;
+    };
+    defer distances.deinit();
+    const time_slice = try times.toOwnedSlice();
+    defer allocator.free(time_slice);
+    const dist_slice = try distances.toOwnedSlice();
+    defer allocator.free(dist_slice);
+    const time = try std.fmt.parseInt(usize, time_slice, 10);
+    const dist = try std.fmt.parseInt(usize, dist_slice, 10);
+    var ways: usize = 0;
+    for (1..time) |option| {
+        const traveled = (option * (time - option));
+        if (traveled > dist) {
+            ways += 1;
+        }
+    }
+    if (ways > 0) {
+        product *= ways;
+    }
+    return product;
+}
+
 test "example one" {
     try std.testing.expectEqual(part1(
         \\Time:      7  15   30
@@ -62,5 +116,16 @@ test "example one" {
 }
 
 test "input one" {
-    try std.testing.expectEqual(part1(@embedFile("day06.txt")), 800280);
+    try std.testing.expectEqual(part1(@embedFile("day06.txt")), 800_280);
+}
+
+test "example two" {
+    try std.testing.expectEqual(part2(
+        \\Time:      7  15   30
+        \\Distance:  9  40  200
+    ), 71503);
+}
+
+test "input two" {
+    try std.testing.expectEqual(part2(@embedFile("day06.txt")), 45_128_024);
 }
